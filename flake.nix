b0398f34c024feafc99a2ca580cbd70c7f2d828e
bernelius <bob@bernelius.com>
2026-08-28 19:32:37 +0200

    flake, direnv

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "ReaScripts dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          ruby
+          bundler
+          cmake
+          gnumake
+          gcc
+          git
+          libgit2
+          libxml2
+          zlib
+          openssl
+          pkg-config
+        ];
+
+        shellHook = ''
+          export GEM_HOME="$PWD/.gems"
+          export PATH="$GEM_HOME/bin:$PATH"
+        '';
+      };
+    };
+}
